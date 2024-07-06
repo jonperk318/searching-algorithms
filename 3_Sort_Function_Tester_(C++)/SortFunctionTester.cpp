f19f598a2d163fe6affd2b1eb978fdd1f227ac1e
Jonathan Perkins <jperkin4@uab.edu>
2024-07-05 23:55:20 -0500

    File IO and measuring run time

--- 3_Sort_Function_Tester_(C++)/SortFunctionTester.cpp
+++ 3_Sort_Function_Tester_(C++)/SortFunctionTester.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <vector>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
 
 // SORTING FUNCTIONS
 
 // Merge Sort
 
-void merge(int arr[], int first, int mid, int last) 
+void merge(vector<int> arr, int first, int mid, int last) 
     // helper function that merges left and right halves of arr[]
 {
     int const leftLength = mid - first + 1; 
     int const rightLength = last - mid; 
 
     // Create temp arrays
-    auto *leftArray = new int[leftLength], *rightArray = new int[rightLength];
+    vector<int> leftArray(leftLength), rightArray(rightLength);
 
-    // Copy data to temp arrays leftArray[] and rightArray[]
-    for (auto i = 0; i < leftLength; i++) 
+    // Copy data to temp arrays
+    for (int i = 0; i < leftLength; i++) 
         leftArray[i] = arr[first + i]; // first subarray is arr[first...mid]
-    for (auto j = 0; j < rightLength; j++)
-        rightArray[j] = arr[mid + 1 + j]; // second subarray is arr[mid+1..last]
+    for (int j = 0; j < rightLength; j++)
+        rightArray[j] = arr[mid + 1 + j]; // second subarray is arr[mid+1...last]
 
-    auto indexOfLeftLength = 0, indexOfRightLength = 0;
+    int indexOfLeftLength = 0, indexOfRightLength = 0;
     int indexOfMergedArray = first;
 
     // Merge temp arrays back into single array
@@ -44,8 +46,7 @@ void merge(int arr[], int first, int mid, int last)
         indexOfMergedArray++;
     }
 
-    // Copy the remaining elements of
-    // left[], if there are any
+    // Copy the remaining elements of left, if there are any
     while (indexOfLeftLength < leftLength) {
         arr[indexOfMergedArray]
             = leftArray[indexOfLeftLength];
@@ -53,35 +54,31 @@ void merge(int arr[], int first, int mid, int last)
         indexOfMergedArray++;
     }
 
-    // Copy the remaining elements of
-    // right[], if there are any
+    // Copy the remaining elements of right, if there are any
     while (indexOfRightLength < rightLength) {
         arr[indexOfMergedArray]
             = rightArray[indexOfRightLength];
         indexOfRightLength++;
         indexOfMergedArray++;
     }
-    delete[] leftArray;
-    delete[] rightArray;
 }
 
-void mergeSort(int arr[], int const first, int const last)
+void mergeSort(vector<int> arr, int const first, int const last)
     // divide and conquer algorithm with O(nlog(n)) time complexity
 {
-    if (first >= last) { // the array cannot be empty or have negative length
-        cout << "Error: length of array has to be greater than 0";
+    if (first >= last) { // base case is an empty array
         return;
         }
 
     int mid = first + (last - first) / 2;
-    mergeSort(arr, first, mid);
-    mergeSort(arr, mid + 1, last);
+    mergeSort(arr, first, mid); // first half of array
+    mergeSort(arr, mid + 1, last); // second half of array
     merge(arr, first, mid, last);
 }
 
 // Quick Sort
 
-int partition(int arr[], int first, int last)
+int partition(vector<int> arr, int first, int last)
   // helper function to choose the pivot
 {
   int pivot = arr[last]; // last element is initially selected as the pivot
@@ -89,8 +86,7 @@ int partition(int arr[], int first, int last)
   
   for(int j = first; j <= last - 1; j++)
   {
-    // check if each element is smaller than the pivot
-    if(arr[j] < pivot)
+    if(arr[j] < pivot) // check if each element is smaller than the pivot
     {
       i++; //if so, increment index of smaller element
       swap(arr[i], arr[j]);
@@ -100,11 +96,10 @@ int partition(int arr[], int first, int last)
   return i + 1; // return index of pivot
 }
            
-void quickSort(int arr[], int first, int last)
+void quickSort(vector<int> arr, int first, int last)
     // divide and conquer algorithm with O(nlog(n)) time complexity
 {
-    if (first >= last) { // the array cannot be empty or have negative length
-        cout << "Error: length of array has to be greater than 0" << endl;
+    if (first >= last) { // base case is an empty array
         return;
         }
 
@@ -116,8 +111,9 @@ void quickSort(int arr[], int first, int last)
 
 // UTILITY FUNCTIONS
 
-void printArray(int arr[], int size) // prints all elements of an array
+void printArray(vector<int> arr) // prints all elements of an array
 {
+    int size = arr.size();
     for (int i = 0; i < size; i++)
         cout << arr[i] << " ";
     cout << endl;
@@ -128,25 +124,27 @@ void printArray(int arr[], int size) // prints all elements of an array
 
 int main()
 {
-    int testArr[] = { 19, 201, 202, 9, 3, 14, 63, 1001 };
-    int testArr2[] = { 19, 201, 202, 9, 3, 14, 63, 1001 };
-    int test_arr_size = sizeof(testArr) / sizeof(testArr[0]);
+    vector<int> testArr = {19, 201, 202, 9, 3, 14, 63, 1001};
+    vector<int> testArr2 = testArr;
+    int testArrSize = testArr.size();
 
     cout << "Given array is \n";
-    printArray(testArr, test_arr_size);
+    printArray(testArr);
 
-    mergeSort(testArr, 0, test_arr_size - 1);
-    quickSort(testArr2, 0, test_arr_size - 1);
+    mergeSort(testArr, 0, testArrSize - 1);
+    quickSort(testArr2, 0, testArrSize - 1);
 
     cout << "\nArray sorted by Merge Sort: \n";
-    printArray(testArr, test_arr_size);
+    printArray(testArr);
     cout << "\nArray sorted by Quick Sort: \n";
-    printArray(testArr2, test_arr_size);
+    printArray(testArr2);
+    cout << endl;
 
 
     // Reading input texts
 
-    int sampleSizes[10] = {1000, 2500, 5000, 10000, 25000, 50000, 100000, 250000, 500000, 1000000};
+    vector<int> sampleSizes = {1000, 2500, 5000, 10000, 25000, 50000, 100000, 250000, 500000, 1000000};
+    vector<vector<int> > inputs1(sampleSizes.size());
 
     for (int size : sampleSizes) {
 
@@ -154,14 +152,63 @@ int main()
         ifstream inputFile("" + str + ".txt");
 
         if (!inputFile.is_open()) { // there should be no error opening the file
-        cerr << "Error opening the file!" << endl; 
-        return 1; 
+            cerr << "Error opening the file!" << endl; 
+            return 1; 
         }
 
-        string line;
+        vector<string> result; // initialize array to read the strings of numbers into
 
+        while (inputFile.good()) {
+            string substr;
+            getline(inputFile, substr, ','); // split each string by the "," delineator
+            result.push_back(substr);
+        }
 
+        vector<int> values; // initialize array to store integers
+
+        for(int i = 0; i < result.size() ; i++) {
+            values[i] = stoi(result[i]); // convert string to integer
+        }
+
+        inputs1.push_back(values); // add unsorted array to array of inputs
     }
+
+    vector<vector<int> > inputs2 = inputs1;
+    
+
+    // Testing sorting functions
+
+    vector<double> mergeSortTimes;
+    vector<double> quickSortTimes;
+
+    for (vector<int> input : inputs1) { // testing mergeSort times
+        auto tick = high_resolution_clock::now();
+        mergeSort(input, 0, input.size());
+        auto tock = high_resolution_clock::now();
+        auto duration = duration_cast<seconds>(tock - tick);
+        mergeSortTimes.push_back(duration.count());
+    }
+
+    for (vector<int> input : inputs2) { // testing quickSort times
+        auto tick = high_resolution_clock::now();
+        mergeSort(input, 0, input.size());
+        auto tock = high_resolution_clock::now();
+        auto duration = duration_cast<seconds>(tock - tick);
+        quickSortTimes.push_back(duration.count());
+    }
+
+    for (double i : mergeSortTimes) {
+        cout << i << " ";
+    }
+    cout << endl;
+    for (double i : quickSortTimes) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    printArray(inputs1[0]);
+    printArray(inputs2[0]);
+
 
     return 0;
 }
